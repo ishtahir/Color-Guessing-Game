c6657746e5988fc146f270c66c44df23c711ad7c
Ish Tahir <ishtahir@icloud.com>
2018-11-06 19:06:43 -0600

    Added selected class/dynamically creating squares/removed code from html

--- rgbgame.js
+++ rgbgame.js
@@ -1,10 +1,11 @@
-const squares = document.querySelectorAll('.square');
 const colorDisplay = document.querySelector('.color-display');
+const container = document.querySelector('.container');
 const playAgainBtn = document.querySelector('.play-again-btn');
 const easyBtn = document.querySelector('.easy-btn');
 const mediumBtn = document.querySelector('.medium-btn');
 const h1 = document.querySelector('h1');
 const messageStatus = document.querySelector('.message-status');
+let squares = createSquares(6);
 let randomIndex = Math.floor(Math.random() * 6);
 let colors = randomColor(6);
 let pickedColor = colors[randomIndex];
@@ -19,6 +20,17 @@ function randomColor(num) {
         colorsArr.push(`rgb(${red}, ${green}, ${blue})`);
     }
     return colorsArr;
+}
+
+function createSquares(num) {
+    const squaresArr = [];
+    for (let i = 0; i < num; i++) {
+        const div = document.createElement('div');
+        div.classList.add('square');
+        squaresArr.push(div);
+        container.appendChild(div);
+    }
+    return squaresArr;
 }
 
 colorDisplay.textContent = pickedColor;
